9a6f4c4bfd5581c341b0e1e58bac80f407301585
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-07-06 11:09:29 +0200

    allow only creation and destruction, redirct back

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,5 +3,15 @@
 
 class SubscriptionsController < Spree::BaseController
   resource_controller
-  actions :all, :only => [:create, :update, :destroy]
+  actions :all, :only => [:create, :destroy]
+
+  create.after do
+    flash[:notice] = I18n.t(:subscribed)
+    redirect_to :back
+  end
+  
+  destroy.after do
+    flash[:notice] = I18n.t(:unsubscribed)
+    redirect_to :back
+  end
 end
