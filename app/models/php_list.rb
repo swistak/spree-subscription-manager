53461b27445490eac65ea8e76a04a562ee15f9b4
kkempin <krzysztof@comgroup.pl>
2011-01-11 12:17:18 +0100

    #1371

---
+++ app/models/php_list.rb
@@ -0,0 +1,35 @@
+require 'md5'
+
+class PhpList < ActiveRecord::Base
+  establish_connection(
+        :adapter  => "mysql",
+        :host     => "localhost",
+        :username => "phplist",
+        :password => "do3Aepho",
+        :database => "phplist"
+      )
+
+  def self.create email
+    srand()
+    uniq_id = MD5.md5("#{rand()}_#{rand()}_#{Time.now.to_i}").to_s 
+    begin
+      a = self.connection.insert_sql("INSERT into phplist_user_user set email = '#{email}',
+      entered = now(), password = NULL,disabled = 0,confirmed = 1, uniqid = '#{uniq_id}',htmlemail = 0")
+      self.connection.insert("INSERT into phplist_listuser set userid = #{a}, listid = 3, entered = now()")
+      true
+    rescue
+      false
+    end
+  end
+
+  def self.destroy email
+    begin
+      a = self.connection.select_one("select id from phplist_user_user where email like '#{email}'")
+      self.connection.delete("delete from phplist_user_user where email like '#{email}'")
+      self.connection.delete("delete from phplist_listuser where userid = #{a['id']}")
+      true
+    rescue
+      false
+    end
+  end
+end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,18 @@ class Subscription < ActiveRecord::Base
   belongs_to :mailing_list
 
   validates_format_of :email, :with => Format::EMAIL_REGEX, :allow_nil => true
+  validates_uniqueness_of :email
+
+  after_create :insert_to_php_list
+  after_destroy :remove_from_php_list
+
+  # insert subscriber into php list database
+  def insert_to_php_list
+    PhpList.create(self.email) 
+  end
+
+  # remove subscriber from php list database
+  def remove_from_php_list
+    PhpList.destroy(self.email)
+  end
 end
